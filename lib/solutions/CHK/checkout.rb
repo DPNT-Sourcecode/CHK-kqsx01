0f3753c9bf2db9cf03eccd3d5e6fa2c1f1a47ea1
sbx_user1051 <sbx_user1051@169.254.111.61>
2019-04-09 10:44:07 +0000

    Tue Apr 09 10:44:07 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -2,7 +2,7 @@
 class Checkout
   def initialize
     @price_table = {"A" => 50, "B" => 30, "C" => 20, "D" => 15}
-    @offers = {"A" => {3 => 130}}
+    @offers = {"A" => [3, 130]}
   end
 
   def checkout(skus) 
@@ -21,8 +21,21 @@ class Checkout
 
   def costs(skus)
     return skus.chars.uniq.map { |char| 
-      skus.count(char) * @price_table[char] 
+      if @offers[char] == nil
+        skus.count(char) * @price_table[char] 
+      else
+        offer_cost(char, skus.count(char))
+      end
     }
   end
 
+  def offer_cost(sku, qty)
+    offer = @offers[sku]
+    offer_qty = offer[0]
+    return qty * @price_table[sku] if qty < offer_qty
+    offer_cost = offer[1] * (qty / offer_qty)
+    non_offer_cost = @price_table[sku] * (qty % offer_qty)
+    return offer_cost + non_offer_cost
+  end
 end
+
