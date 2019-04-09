5bea6e34eaa3e0904f5e3cda9e9a6829132ceffd
sbx_user1051 <sbx_user1051@169.254.196.241>
2019-04-09 11:32:21 +0000

    Tue Apr 09 11:32:21 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -2,7 +2,7 @@
 class Checkout
   def initialize
     @price_table = {"A" => 50, "B" => 30, "C" => 20, "D" => 15}
-    @offers = {"A" => [3, 130], "B" => [2, 45]}
+    @offers = {"A" => [[5, 200], [3, 130]], "B" => [2, 45]}
   end
 
   def checkout(skus) 
@@ -27,9 +27,18 @@ class Checkout
   end
 
   def offer_cost(sku, qty)
-    offer_qty = @offers[sku][0]
-    offer_cost = @offers[sku][1] * (qty / offer_qty)
-    non_offer_cost = @price_table[sku] * (qty % offer_qty)
+    offers = @offers[sku]
+    offer_cost = 0
+    non_offer_qty = offers.reduce(qty) { |rem, offer| 
+      if qualify?(offer, rem)
+        offer_cost += offer[1] * (rem / offer[0])
+      end
+      rem % offer[0]
+    }
+    # go through each offer, starting with biggest qty, calc offer cost and pass the remainder as the qty for looking at the next offer
+
+    non_offer_cost = @price_table[sku] * non_offer_qty
     return offer_cost + non_offer_cost
   end
 end
+
