8fbbacba6b12fc073d3a63c8461424083434f71c
sbx_user1051 <sbx_user1051@169.254.111.61>
2019-04-09 09:44:07 +0000

    Tue Apr 09 09:44:07 UTC 2019

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,6 +1,11 @@
 require './lib/solutions/CHK/checkout.rb'
 
 describe Checkout do
-    it '' do
+    it 'returns 15 when given D' do
+        c = Checkout.new
+        expect(c.checkout("D")).to eq(15)
     end
+
+    # For any illegal input return -1
+    
 end
