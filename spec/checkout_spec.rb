cf2f65ddb13f8201fe473e24cf502f8921d01c74
sbx_user1051 <sbx_user1051@169.254.89.13>
2019-04-09 17:33:21 +0000

    Tue Apr 09 17:33:21 UTC 2019

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -170,24 +170,48 @@ describe Checkout do
         c = Checkout.new
         expect(c.checkout("Z")).to eq(50)
     end
-#     G    | 20    |                        |
-# | H    | 10    | 5H for 45, 10H for 80  |
-# | I    | 35    |                        |
-# | J    | 60    |                        |
-# | K    | 80    | 2K for 150             |
-# | L    | 90    |                        |
-# | M    | 15    |                        |
+
+    it 'returns 45 when given HHHHH' do
+        c = Checkout.new
+        expect(c.checkout("HHHHH")).to eq(45)
+    end
+
+    it 'returns 80 when given HHHHHHHHHH' do
+        c = Checkout.new
+        expect(c.checkout("HHHHHHHHHH")).to eq(80)
+    end
+
+    it 'returns 150 when given KK' do
+        c = Checkout.new
+        expect(c.checkout("KK")).to eq(150)
+    end
+
+    it 'returns 200 when given PPPPP' do
+        c = Checkout.new
+        expect(c.checkout("PPPPP")).to eq(200)
+    end
+
+    it 'returns 80 when given QQQ' do
+        c = Checkout.new
+        expect(c.checkout("QQQ")).to eq(80)
+    end
+
+    it 'returns 120 when given UUUU' do
+        c = Checkout.new
+        expect(c.checkout("UUUU")).to eq(120)
+    end
+
+    it 'returns 90 when given VV' do
+        c = Checkout.new
+        expect(c.checkout("VV")).to eq(90)
+    end
+
+    it 'returns 130 when given VVV' do
+        c = Checkout.new
+        expect(c.checkout("VVV")).to eq(130)
+    end
 # | N    | 40    | 3N get one M free      |
-# | O    | 10    |                        |
-# | P    | 50    | 5P for 200             |
-# | Q    | 30    | 3Q for 80              |
+
 # | R    | 50    | 3R get one Q free      |
-# | S    | 30    |                        |
-# | T    | 20    |                        |
-# | U    | 40    | 3U get one U free      |
-# | V    | 50    | 2V for 90, 3V for 130  |
-# | W    | 20    |                        |
-# | X    | 90    |                        |
-# | Y    | 10    |                        |
-# | Z    | 50    | 
+
 end
