a7b4fc55b9e896035bb3d388e94fcc23810a3a98
sbx_user1051 <sbx_user1051@169.254.111.45>
2019-04-09 15:30:10 +0000

    Tue Apr 09 15:30:10 UTC 2019

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,68 +1,68 @@
 require './lib/solutions/CHK/checkout.rb'
 
 describe Checkout do
-    # it 'returns 15 when given D' do
-    #     c = Checkout.new
-    #     expect(c.checkout("D")).to eq(15)
-    # end
+    it 'returns 15 when given D' do
+        c = Checkout.new
+        expect(c.checkout("D")).to eq(15)
+    end
 
-    # it 'returns 30 when given DD' do
-    #     c = Checkout.new
-    #     expect(c.checkout("DD")).to eq(30)
-    # end
+    it 'returns 30 when given DD' do
+        c = Checkout.new
+        expect(c.checkout("DD")).to eq(30)
+    end
 
-    # it 'returns 50 when given DCD' do
-    #     c = Checkout.new
-    #     expect(c.checkout("DCD")).to eq(50)
-    # end
+    it 'returns 50 when given DCD' do
+        c = Checkout.new
+        expect(c.checkout("DCD")).to eq(50)
+    end
 
-    # it 'returns 130 when given ABDCD' do
-    #     c = Checkout.new
-    #     expect(c.checkout("ABDCD")).to eq(130)
-    # end
+    it 'returns 130 when given ABDCD' do
+        c = Checkout.new
+        expect(c.checkout("ABDCD")).to eq(130)
+    end
 
-    # it 'returns -1 when given Z' do
-    #     c = Checkout.new
-    #     expect(c.checkout("Z")).to eq(-1)
-    # end
+    it 'returns -1 when given Z' do
+        c = Checkout.new
+        expect(c.checkout("Z")).to eq(-1)
+    end
 
-    # it 'returns -1 when given ABCDZ' do
-    #     c = Checkout.new
-    #     expect(c.checkout("ABCDZ")).to eq(-1)
-    # end
+    it 'returns -1 when given ABCDZ' do
+        c = Checkout.new
+        expect(c.checkout("ABCDZ")).to eq(-1)
+    end
 
-    # it 'returns 130 when given AAA' do
-    #     c = Checkout.new
-    #     expect(c.checkout("AAA")).to eq(130)
-    # end
+    it 'returns 130 when given AAA' do
+        c = Checkout.new
+        expect(c.checkout("AAA")).to eq(130)
+    end
 
-    # it 'returns 200 when given AAAAA' do
-    #     c = Checkout.new
-    #     expect(c.checkout("AAAAA")).to eq(200)
-    # end
+    it 'returns 200 when given AAAAA' do
+        c = Checkout.new
+        expect(c.checkout("AAAAA")).to eq(200)
+    end
 
-    # it 'returns 380 when given AAAAAAAAA' do
-    #     c = Checkout.new
-    #     expect(c.checkout("AAAAAAAAA")).to eq(380)
-    # end
+    it 'returns 380 when given AAAAAAAAA' do
+        c = Checkout.new
+        expect(c.checkout("AAAAAAAAA")).to eq(380)
+    end
 
-    # it 'returns 45 when given BB' do
-    #     c = Checkout.new
-    #     expect(c.checkout("BB")).to eq(45)
-    # end
+    it 'returns 45 when given BB' do
+        c = Checkout.new
+        expect(c.checkout("BB")).to eq(45)
+    end
 
-    # it 'returns 290 when given AAAABBBCD' do
-    #     c = Checkout.new
-    #     expect(c.checkout("AAAABBBCD")).to eq(290)
-    # end
+    it 'returns 290 when given AAAABBBCD' do
+        c = Checkout.new
+        expect(c.checkout("AAAABBBCD")).to eq(290)
+    end
 
-    # it 'returns 80 when given BEE' do
-    #     c = Checkout.new
-    #     expect(c.checkout("BEE")).to eq(80)
-    # end
+    it 'returns 80 when given BEE' do
+        c = Checkout.new
+        expect(c.checkout("BEE")).to eq(80)
+    end
 
     it 'returns 190 when given BBBEEEE' do
         c = Checkout.new
-        expect(c.checkout("BBBEEEE")).to eq(190)
+        expect(c.checkout("BBBEEEE")).to eq(175)
     end
 end
